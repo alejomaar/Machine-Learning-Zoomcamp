8b08ca48d9153753c0fc04eb8a9fa6b6c993e4bc
alejomaar <demonmaar@gmail.com>
2022-10-09 19:55:11 -0500

    ADD expose service in Docker

--- Week 5/request_test.py
+++ Week 5/request_test.py
@@ -2,7 +2,9 @@ import requests
 
 url = 'http://localhost:9696/predict'
 
-customer ={"reports": 0, "share": 0.001694, "expenditure": 0.12, "owner": "yes"}
+#customer ={"reports": 0, "share": 0.001694, "expenditure": 0.12, "owner": "yes"}
+customer ={"reports": 0, "share": 0.245, "expenditure": 3.438, "owner": "yes"}
+
 customer_id = 'xyz-123'
 
 response = requests.post(url, json=customer).json()
