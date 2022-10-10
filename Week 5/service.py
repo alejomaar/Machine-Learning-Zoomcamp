707ff2323d311d36c6ef3cfaa2714dc6992fdb4a
alejomaar <demonmaar@gmail.com>
2022-10-09 20:38:17 -0500

    ADD flask service for different models

--- Week 5/service.py
+++ Week 5/service.py
@@ -4,25 +4,46 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 
-model_file = 'model1.bin'
-dv_file = 'dv.bin'
 
-with open(model_file,'rb') as f:
-    model = pickle.load(f)
-    
-with open(dv_file,'rb') as f:
-    dv = pickle.load(f)
+def get_model(model_file,dv_file):
+    with open(model_file,'rb') as f:
+        model = pickle.load(f)    
+    with open(dv_file,'rb') as f:
+        dv = pickle.load(f)
+    return model,dv
 
 app = Flask('credit')
 
+#Customers can have a credit card `(1)` or not `(0)`.
+#The order of the prediction has the form:
+#[`Probability of not getting a credit card`, `Probability of getting a credit card`]
+
 @app.route('/predict', methods=['POST'])
 def predict():
+    #Predict if customer gets a credit card with model1
+    model,dv = get_model('model1.bin','dv.bin')
     customer = request.get_json()
 
     X = dv.transform([customer])
-    #Customers can have a credit card `(1)` or not `(0)`.
-    #The order of the prediction has the form:
-    #[`Probability of not getting a credit card`, `Probability of getting a credit card`]
+    
+    y_pred = model.predict_proba(X)[0, 1]
+    is_credit = y_pred >= 0.5
+
+    result = {
+        'credit_probability': float(y_pred),
+        'is_credit': bool(is_credit)
+    }
+
+    return jsonify(result)
+
+
+@app.route('/predict2', methods=['POST'])
+def predict2():
+    #Predict if customer gets a credit card with model2
+    model,dv = get_model('model2.bin','dv.bin')
+    customer = request.get_json()
+
+    X = dv.transform([customer])
     
     y_pred = model.predict_proba(X)[0, 1]
     is_credit = y_pred >= 0.5
