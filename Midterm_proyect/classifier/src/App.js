0efcb74e9e12bf890c5e2139579ba3d4132876f2
alejomaar <demonmaar@gmail.com>
2022-11-05 19:16:16 -0500

    Build frontend app

--- Midterm_proyect/classifier/src/App.js
+++ Midterm_proyect/classifier/src/App.js
@@ -1,42 +1,82 @@
 import "./App.css";
 
+const questions = [
+  {
+    description: "Cries when feels down or things are not going well",
+    name: "life_struggles",
+  },
+  { description: "like PC software, hardware", name: "pc" },
+  { description: "like shopping", name: "shopping" },
+  { description: "like war movies", name: "war" },
+  { description: "like action movies", name: "action" },
+  { description: "like cars ", name: "cars" },
+  {
+    description: "like science and technology",
+    name: "science_and_technology",
+  },
+  { description: "like romantic movies", name: "romantic" },
+  { description: "like poetry reading", name: "reading" },
+  { description: "like western movies", name: "western" },
+  { description: "like dancing", name: "dancing" },
+  { description: "like theatre", name: "theatre" },
+  { description: "Are you afraid of darkness?", name: "darkness" },
+];
+
 function App() {
   const submit = () => {
-    var x = document.querySelector("input[name=t]:checked");
-    console.log(x.value);
+    let response = questions.reduce((acc, { name }) => {
+      const input = document.querySelector(`input[name=${name}]:checked`);
+      return {
+        ...acc,
+        [name]: +input?.value,
+      };
+    }, {});
+    response["height"] = +document.getElementById("height")?.value;
+    response["weight"] = +document.getElementById("weight")?.value;
+
+    console.log(response);
   };
 
   return (
     <div className="app d-flex flex-column justify-content-center">
       <h1>Gender Classificator</h1>
       <div className="card d-flex flex-column align-items-center">
-        <label htmlFor="">Cual es tu peso(kg)</label>
-        <div>
-          <input type="number" />
+        <label htmlFor="">What is your height(cm)?</label>
+        <div style={{ marginTop: "0.5rem" }}>
+          <input id="height" type="number" />
         </div>
       </div>
       <div className="card d-flex flex-column align-items-center">
-        <label htmlFor="">Cual es tu altura(cm)</label>
-        <div>
-          <input type="number" />
+        <label htmlFor="">What is your weight(kg)?</label>
+        <div style={{ marginTop: "0.5rem" }}>
+          <input id="weight" type="number" />
         </div>
       </div>
-      <div className="card d-flex flex-column align-items-center">
-        <label htmlFor="">Te gustan las peliculas</label>
-        <div className="d-flex">
-          <span>Disagree</span>
-          <div>
-            <input type="radio" name="t" id="" value={1} />
-            <input type="radio" name="t" id="" value={2} />
-            <input type="radio" name="t" id="" value={3} />
-            <input type="radio" name="t" id="" value={4} />
-            <input type="radio" name="t" id="" value={5} />
+      {questions.map(({ description, name }) => (
+        <div className="card d-flex flex-column align-items-center">
+          <label htmlFor="">{description}</label>
+          <div style={{ marginTop: "0.5rem" }} className="d-flex">
+            <span className="warning">Disagree</span>
+            <div>
+              <input type="radio" name={name} id="" value={1} />
+              <input type="radio" name={name} id="" value={2} />
+              <input type="radio" name={name} id="" value={3} />
+              <input type="radio" name={name} id="" value={4} />
+              <input type="radio" name={name} id="" value={5} />
+            </div>
+            <span className="ok">Agree</span>
           </div>
-          <span>Agree</span>
         </div>
-      </div>
+      ))}
 
-      <button onClick={() => submit()}>Submit</button>
+      <div
+        className="d-flex justify-content-center"
+        style={{ width: "100%", margin: "1rem 0rem" }}
+      >
+        <button className="btn" onClick={() => submit()}>
+          Submit
+        </button>
+      </div>
     </div>
   );
 }
